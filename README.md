6ab4c1f2729bc668561a398e38048819802e58e6
Constant <delikates@mail.ru>
2018-07-30 12:27:14 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tree Dropdown
+
+English:
+
+Tree List.
+This is extension for Yii2 framework. 
+
+
---
+++ TreeListAsset.php
@@ -0,0 +1,16 @@
+<?php
+namespace delikatesnsk\treelist;
+
+use yii\web\AssetBundle;
+
+class TreeListAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/delikates-nsk/yii2-tree-list/assets';
+
+    public $js = [
+    ];
+
+    public $css = [
+        'css/treelist.css?v=1',
+    ];
+}
